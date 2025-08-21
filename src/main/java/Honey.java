4d4273534e1db7f40f5ba8c65903e46f70442ae7
brid-get-it <bridget@healthgenplus.com>
2025-08-21 16:02:48 +0800

    Level 0. Rename, Greet, Exit Change name of chatbot to Honey.  Implement an initial skeletal version of it that simply greets the user and exits. Add horizontal lines.

---
+++ src/main/java/Honey.java
@@ -0,0 +1,13 @@
+public class Honey {
+    public static void main(String[] args) {
+        printBreak();
+        System.out.println("Hello! I'm Honey\n" + "What can I do for you?");
+        printBreak();
+        System.out.println("Bye. Hope to see you again soon!");
+        printBreak();
+    }
+    
+    public static void printBreak() {
+        System.out.println("____________________________________________________________");
+    }
+}
